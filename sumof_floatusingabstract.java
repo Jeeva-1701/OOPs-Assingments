c7af698a93c9a7f2c9b0f3b86efe761b8fc5b8d8
Jeeva-1701 <89633224+Jeeva-1701@users.noreply.github.com>
2022-01-05 09:28:48 +0530

    Create sumof_floatusingabstract.java

---
+++ sumof_floatusingabstract.java
@@ -0,0 +1,45 @@
+//Program to Add floating numbers using abstract class
+
+abstract class  summationofloatusingabstract {
+   abstract void add_tion();
+}
+public class summationofloatusingabstract extends assal{
+    public void add_tion(){
+        float a=00.0456456453f;
+        float b=0.0022332f;
+        System.out.println(a+b);
+    }
+    public static void main(String args[])
+    {
+        summationofloatusingabstract o=new summationofloatusingabstract();
+        o.add_tion();
+    }
+}{
+   abstract void add_tion();
+}
+public class summationofloatusingabstract extends assal{
+    public void add_tion(){
+        float a=00.0456456453f;
+        float b=0.0022332f;
+        System.out.println(a+b);
+    }
+    public static void main(String args[])
+    {
+        summationofloatusingabstract o=new summationofloatusingabstract();
+        o.add_tion();
+    }
+}{
+   abstract void add_tion();
+}
+public class summationofloatusingabstract extends assal{
+    public void add_tion(){
+        float a=00.0456456453f;
+        float b=0.0022332f;
+        System.out.println(a+b);
+    }
+    public static void main(String args[])
+    {
+        summationofloatusingabstract o=new summationofloatusingabstract();
+        o.add_tion();
+    }
+}
